0e8fdf87bee55199d43234efb364ac676676f1f4
Kevin Donahue <nonnontrivial@gmail.com>
2019-07-22 14:22:16 -0400

    write generated json file size

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-// import * as utils from '@botmock-api/utils'
-import Botmock from 'botmock';
-import chalk from 'chalk';
-import fs from 'fs';
-import Provider from './lib/Provider';
-import { getArgs, parseVar, toDashCase } from './util';
-import "dotenv/config"
+import "dotenv/config";
+import fs from "fs";
+import os from "os";
+import Botmock from "botmock";
+import Provider from "./lib/Provider";
+import { getArgs, parseVar, toDashCase } from "./util";
 
 const OUTPUT_PATH = `${process.cwd()}/out`;
+
 try {
   await fs.promises.access(OUTPUT_PATH, fs.constants.R_OK);
 } catch (_) {
@@ -18,7 +18,7 @@ const {
   BOTMOCK_TOKEN,
   BOTMOCK_TEAM_ID,
   BOTMOCK_PROJECT_ID,
-  BOTMOCK_BOARD_ID
+  BOTMOCK_BOARD_ID,
 } = process.env;
 
 const { isInDebug: debug, hostname: url } = getArgs(process.argv);
@@ -28,9 +28,9 @@ const project = await client.projects(BOTMOCK_TEAM_ID, BOTMOCK_PROJECT_ID);
 // Map an intent to its correct object representation
 const getIntentCollection = intent => ({
   intent: toDashCase(intent.name),
-  examples: intent.utterances.map(u => ({ text: u.text || '_' })),
+  examples: intent.utterances.map(u => ({ text: u.text || "_" })),
   created: intent.created_at.date,
-  updated: intent.updated_at.date
+  updated: intent.updated_at.date,
 });
 
 // Map an entity to its correct object representation
@@ -39,39 +39,45 @@ const getEntitiesCollection = entity => ({
   created: entity.created_at.date,
   updated: entity.updated_at.date,
   values: entity.data.map(({ value, synonyms }) => ({
-    type: 'synonyms',
+    type: "synonyms",
     value,
     synonyms: !Array.isArray(synonyms)
-      ? synonyms.map(toDashCase).split(',')
-      : synonyms
-  }))
+      ? synonyms.map(toDashCase).split(",")
+      : synonyms,
+  })),
 });
 
-// Write a single json file containing the project data
 try {
   const template = await fs.promises.readFile(
     `${process.cwd()}/template.json`,
-    'utf8'
+    "utf8"
   );
   const deserializedTemplate = JSON.parse(template);
+  const filepath = `${OUTPUT_PATH}/${toDashCase(project.name)}.json`;
   await fs.promises.writeFile(
-    `${OUTPUT_PATH}/${toDashCase(project.name)}.json`,
-    JSON.stringify({
-      ...deserializedTemplate,
-      dialog_nodes: await getDialogNodes(project.platform),
-      intents: (await client.intents(BOTMOCK_TEAM_ID, BOTMOCK_PROJECT_ID)).map(
-        getIntentCollection
-      ),
-      entities: (await client.entities(
-        BOTMOCK_TEAM_ID,
-        BOTMOCK_PROJECT_ID
-      )).map(getEntitiesCollection),
-      created: project.created_at.date,
-      updated: project.updated_at.date,
-      name: project.name
-    })
+    filepath,
+    JSON.stringify(
+      {
+        ...deserializedTemplate,
+        dialog_nodes: await getDialogNodes(project.platform),
+        intents: (await client.intents(
+          BOTMOCK_TEAM_ID,
+          BOTMOCK_PROJECT_ID
+        )).map(getIntentCollection),
+        entities: (await client.entities(
+          BOTMOCK_TEAM_ID,
+          BOTMOCK_PROJECT_ID
+        )).map(getEntitiesCollection),
+        created: project.created_at.date,
+        updated: project.updated_at.date,
+        name: project.name,
+      },
+      null,
+      2
+    ) + os.EOL
   );
-  console.log(chalk.bold('done'));
+  const { size } = await fs.promises.stat(filepath);
+  console.log(`done. ${os.EOL}wrote ${size / 1000}kB to ${filepath}.`);
 } catch (err) {
   console.error(err.stack);
   process.exit(1);
@@ -106,70 +112,73 @@ async function getDialogNodes(platform) {
       previous_sibling = siblings[i - 1];
     }
     // Coerce types to their watson equivalents
-    // (see https://cloud.ibm.com/docs/services/assistant?topic=assistant-dialog-responses-json)
+    // see https://cloud.ibm.com/docs/services/assistant?topic=assistant-dialog-responses-json
     const generic = {};
     switch (message.message_type) {
-      case 'image':
-        generic.response_type = 'image';
+      case "image":
+        generic.response_type = "image";
         generic.source = message.payload.image_url;
         break;
-      case 'button':
-      case 'quick_replies':
+      case "button":
+      case "quick_replies":
         const transformPayload = value => ({
           label: value.title,
           value: {
             input: {
-              text: value.payload
-            }
-          }
+              text: value.payload,
+            },
+          },
         });
-        generic.response_type = 'option';
-        generic.title = message.payload.text || '';
+        generic.response_type = "option";
+        generic.title = message.payload.text || "";
         generic.options = message.payload.hasOwnProperty(message.message_type)
           ? message.payload[message.message_type].map(transformPayload)
           : message.payload[`${message.message_type}s`].map(transformPayload);
         break;
-      case 'text':
-        generic.response_type = 'text';
-        generic.values = [{ text: message.payload.text || '' }];
+      case "text":
+        generic.response_type = "text";
+        generic.values = [{ text: message.payload.text || "" }];
         break;
       default:
         console.warn(
-          chalk.dim(
-            `"${message.message_type}" is an unsupported node type and will be coerced to text`
-          )
+          `"${message.message_type}" is an unsupported node type and will be coerced to text`
         );
-        generic.response_type = 'text';
+        generic.response_type = "text";
         generic.values = [{ text: JSON.stringify(message.payload) }];
     }
     const [{ message_id: nextMessageId } = {}] = message.next_message_ids;
     nodes.push({
       output: {
-        ...(platform === 'slack'
+        ...(platform === "slack"
           ? {
-              [platform]: provider.create(message.message_type, message.payload)
+              [platform]: provider.create(
+                message.message_type,
+                message.payload
+              ),
             }
           : {}),
-        generic: [generic]
+        generic: [generic],
       },
       title: message.payload.nodeName
         ? toDashCase(message.payload.nodeName)
-        : 'welcome',
-      next_step: message.next_message_ids.every(i => !i.action.payload)
+        : "welcome",
+      next_step: message.next_message_ids.every(
+        message => !message.action.payload
+      )
         ? {
-            behavior: 'skip_user_input',
-            selector: 'body',
-            dialog_node: nextMessageId
+            behavior: "skip_user_input",
+            selector: "body",
+            dialog_node: nextMessageId,
           }
         : {
-            behavior: 'jump_to',
-            selector: message.is_root ? 'body' : 'user_input',
-            dialog_node: nextMessageId
+            behavior: "jump_to",
+            selector: message.is_root ? "body" : "user_input",
+            dialog_node: nextMessageId,
           },
       previous_sibling,
       conditions: message.is_root
-        ? 'welcome'
-        : conditionsMap[message.message_id] || 'anything_else',
+        ? "welcome"
+        : conditionsMap[message.message_id] || "anything_else",
       parent: prev.message_id,
       dialog_node: message.message_id,
       context: Array.isArray(message.payload.context)
@@ -177,7 +186,7 @@ async function getDialogNodes(platform) {
             (acc, k) => ({ ...acc, [parseVar(k.name)]: k.default_value }),
             {}
           )
-        : {}
+        : {},
     });
     // Maintain lookup table relating message_id -> intent incident on it
     for (const y of message.next_message_ids) {
