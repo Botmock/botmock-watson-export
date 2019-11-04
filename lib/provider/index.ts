4a4247b9859e4c0dc04d32a6bdb533826b7c61b9
Kevin Donahue <nonnontrivial@gmail.com>
2019-11-04 13:32:26 -0500

    add private method to platform provider

--- lib/provider/index.ts
+++ lib/provider/index.ts
@@ -23,41 +23,50 @@ export default class PlatformProvider extends AbstractProject {
     this.platform = new mod();
   }
   /**
+   * Gets responses for any messages in the same segment as a given message
+   * @param message message to map responses from
+   */
+  private getResponsesForAllMessagesImpliedByFirstMessage(message: Message): unknown {
+    return [message, ...this.gatherMessagesUpToNextIntent(message)].map(message => {
+      const { message_type: contentBlockType } = message;
+      let methodToCallOnClass: string;
+      switch (contentBlockType) {
+        case "api":
+        case "jump":
+          methodToCallOnClass = undefined;
+          break;
+        case "delay":
+          methodToCallOnClass = "pause";
+          break;
+        case "quick_replies":
+        case "button":
+          methodToCallOnClass = "option";
+          break;
+        default:
+          methodToCallOnClass = Object.getOwnPropertyNames(
+            Object.getPrototypeOf(this.platform)).find(prop => contentBlockType.includes(prop)
+            );
+      }
+      if (!methodToCallOnClass) {
+        methodToCallOnClass = "text";
+      }
+      return this.platform[methodToCallOnClass](message.payload);
+    })
+  }
+  /**
    * Creates output field within a dialog node
    * @param message the first message
    * @returns response able to be mixed-in to rest of response object
    */
   public create(message: Message): Partial<{ [key: string]: any }> {
-    const { message_type: contentBlockType } = message;
-    let methodToCallOnClass: string;
-    switch (contentBlockType) {
-      case "api":
-      case "jump":
-        methodToCallOnClass = undefined;
-        break;
-      case "delay":
-        methodToCallOnClass = "pause";
-        break;
-      case "quick_replies":
-      case "button":
-        methodToCallOnClass = "option";
-        break;
-      default:
-        methodToCallOnClass = Object.getOwnPropertyNames(
-          Object.getPrototypeOf(this.platform)).find(prop => contentBlockType.includes(prop)
-        );
-    }
-    if (!methodToCallOnClass) {
-      methodToCallOnClass = "text";
-    }
-    const platfomResponse: unknown = Watson.SupportedPlatforms[this.platform] ? { [this.platform]: {} } : {};
+    const platfomResponse: unknown = Watson.SupportedPlatforms[this.platform]
+      ? { [this.platform]: this.getResponsesForAllMessagesImpliedByFirstMessage(message) }
+      : {};
     return {
       ...platfomResponse,
       selection_policy: Watson.SelectionPolicies.sequential,
-      response_type: methodToCallOnClass,
-      generic: [message, ...this.gatherMessagesUpToNextIntent(message)].map(message => (
-        this.platform[methodToCallOnClass](message.payload)
-      ))
+      // response_type: methodToCallOnClass,
+      generic: this.getResponsesForAllMessagesImpliedByFirstMessage(message),
     };
   }
 }
